4b2b219315fcc255dbb75c6471fe7500f83ad2be
Cymek12 <jak.cymerman@gmail.com>
2025-04-15 10:58:48 +0200

    second commit

---
+++ src/main/java/com/example/repository_details/RepositoryDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.repository_details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryDetailsApplication.class, args);
+	}
+
+}
